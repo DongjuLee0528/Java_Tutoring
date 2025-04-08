41a4497a8508d931139d94d99767147d0ff17f87
wonyoung jung <sancake@naver.com>
2025-04-08 19:04:15 +0900

    수정

--- src/MyJava.java
+++ src/MyJava.java
@@ -3,6 +3,10 @@ public class MyJava {
         System.out.println("202530127 정원영");
         System.out.println("202530127");
         System.out.println("정원영");
+        System.out.println();
+        System.out.println("202530127 정원영");
+        System.out.println("202530127");
+        System.out.println("정원영");
 
     }
 }
