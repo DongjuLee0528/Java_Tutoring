10f19a0192dfd25c435b2b31a6abc000ae774223
kyo1ko <09l354@naver.com>
2025-04-08 19:00:18 +0900

    첫 푸시

---
+++ src/MyJava.java
@@ -0,0 +1,7 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("202530419 임서연");
+        System.out.println("202530419");
+        System.out.println("임서연");
+    }
+}
