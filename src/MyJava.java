4c3d2c066b4d54fa0ef24b4912dae5509cb45710
wonyoung jung <sancake@naver.com>
2025-04-08 19:00:23 +0900

    첫 푸쉬

---
+++ src/MyJava.java
@@ -0,0 +1,8 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("202530127 정원영");
+        System.out.println("202530127");
+        System.out.println("정원영");
+
+    }
+}
