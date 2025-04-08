08f23415ea18ffcbe1f79c242ff0e4bd32ae0d89
seongju2006 <philipbo0523@gmail.com>
2025-04-08 19:01:23 +0900

    첫 푸쉬

---
+++ src/MyJava.java
@@ -0,0 +1,7 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("202530108 박성주");
+        System.out.println("202530108");
+        System.out.println("박성주");
+    }
+}
