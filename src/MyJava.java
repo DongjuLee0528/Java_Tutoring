21bdadfc29dac41d528480bd31caaf191cc8ea00
DongjuLee <dozoo0528@gmail.com>
2025-04-08 19:00:17 +0900

    첫 푸쉬

---
+++ src/MyJava.java
@@ -0,0 +1,11 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("202430219 이동주");
+        System.out.println("202430219");
+        System.out.println("이동주");
+        System.out.println();
+        System.out.println("202430219 이동주");
+        System.out.println("202430219");
+        System.out.println("이동주");
+    }
+}
