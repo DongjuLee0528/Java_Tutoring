2315fae2c4af9604b929038b75a16daabb54c8e3
seoyun <seoyun5658@gmail.com>
2025-04-08 19:00:55 +0900

    2025.04.08 First Push

---
+++ src/MyJava.java
@@ -0,0 +1,11 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("202530115 송서윤");
+        System.out.println("202530115");
+        System.out.println("송서윤");
+        System.out.println();
+        System.out.println("202530115 송서윤");
+        System.out.println("202530115");
+        System.out.println("송서윤");
+    }
+}
