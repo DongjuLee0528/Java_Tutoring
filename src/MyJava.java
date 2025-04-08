376b8833aabeffa00b6107a0cf5277761d1c7efd
이자목 <jmwasd3412@gmail.com>
2025-04-08 19:01:20 +0900

    첫 푸시

---
+++ src/MyJava.java
@@ -0,0 +1,11 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("202530122 이자목");
+        System.out.println("202530122");
+        System.out.println("이자목");
+        System.out.println();
+        System.out.println("202530122 이자목");
+        System.out.println("202530122");
+        System.out.println("이자목");
+    }
+}
