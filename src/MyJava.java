0641eb5069cccaa22da872fae4819d4a95b53d16
kyo1ko <09l354@naver.com>
2025-04-08 19:04:17 +0900

    수정

--- src/MyJava.java
+++ src/MyJava.java
@@ -3,5 +3,9 @@ public class MyJava {
         System.out.println("202530419 임서연");
         System.out.println("202530419");
         System.out.println("임서연");
+        System.out.println();
+        System.out.println("202530419 임서연");
+        System.out.println("202530419");
+        System.out.println("임서연");
     }
 }
